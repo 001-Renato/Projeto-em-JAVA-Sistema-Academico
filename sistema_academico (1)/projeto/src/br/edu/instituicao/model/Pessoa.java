9456d204103a41b010ae8d3ef215a26eb5906a6c
Arthurz11 <arthurbpa29@gmail.com>
2026-05-12 18:09:43 -0300

    Update Pessoa.java

--- sistema_academico (1)/projeto/src/br/edu/instituicao/model/Pessoa.java
+++ sistema_academico (1)/projeto/src/br/edu/instituicao/model/Pessoa.java
@@ -1,18 +1,22 @@
 package br.edu.instituicao.model;
 
+// [AULA] Classe Abstrata: Não pode ser instanciada (não podemos fazer 'new Pessoa()').
+// Serve como um "molde" ou base para outras classes filhas (Aluno, Professor).
 public abstract class Pessoa {
 
+    // [AULA] Encapsulamento: Atributos 'private' só podem ser acessados diretamente de dentro desta própria classe.
     private String nome;
     private String cpf;
     private String email;
 
+    // [AULA] Construtor: Método especial chamado no momento da criação do objeto (new).
     public Pessoa(String nome, String cpf, String email) {
         this.nome = nome;
         this.cpf = cpf;
         this.email = email;
     }
 
-    // Getters e Setters
+    // [AULA] Getters e Setters: Métodos públicos para acessar ou modificar os atributos privados com segurança.
     public String getNome() { return nome; }
     public void setNome(String nome) { this.nome = nome; }
 
@@ -22,8 +26,7 @@ public abstract class Pessoa {
     public String getEmail() { return email; }
     public void setEmail(String email) { this.email = email; }
 
-    @Override
-    public String toString() {
-        return "Nome: " + nome + " | CPF: " + cpf + " | Email: " + email;
-    }
+    // [AULA] Método Abstrato: Não tem corpo (código).
+    // Obriga TODAS as classes filhas a implementarem este comportamento obrigatório!
+    public abstract String exibirDetalhes();
 }

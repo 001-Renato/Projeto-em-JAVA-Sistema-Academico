edb598fc9e7991f61c442cb3d42656b5bb5ff135
Arthurz11 <arthurbpa29@gmail.com>
2026-05-12 18:01:53 -0300

    Update Autenticavel.java

--- sistema_academico (1)/projeto/src/br/edu/instituicao/interfaces/Autenticavel.java
+++ sistema_academico (1)/projeto/src/br/edu/instituicao/interfaces/Autenticavel.java
@@ -1,5 +1,7 @@
 package br.edu.instituicao.interfaces;
 
+// [AULA] Interface: Isola o comportamento de "Autenticação".
+// Isso permite que no futuro até mesmo um "Aluno" possa fazer login se quisermos, bastando implementar esta interface.
 public interface Autenticavel {
     boolean login(String senha);
 }

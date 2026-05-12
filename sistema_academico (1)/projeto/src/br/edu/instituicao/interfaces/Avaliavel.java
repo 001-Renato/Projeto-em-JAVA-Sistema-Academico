300f74a4ad445ae701a41bb29df9582f7860738d
Arthurz11 <arthurbpa29@gmail.com>
2026-05-12 18:02:48 -0300

    Update Avaliavel.java

--- sistema_academico (1)/projeto/src/br/edu/instituicao/interfaces/Avaliavel.java
+++ sistema_academico (1)/projeto/src/br/edu/instituicao/interfaces/Avaliavel.java
@@ -1,5 +1,8 @@
 package br.edu.instituicao.interfaces;
 
+// [AULA] Interface: É como um "contrato". Qualquer classe que assinar este contrato (implementar)
+// será rigorosamente obrigada a fornecer uma lógica para os métodos declarados aqui.
 public interface Avaliavel {
+    // [AULA] Os métodos em uma interface são implicitamente 'public' e 'abstract'.
     double getMediaFinal();
 }

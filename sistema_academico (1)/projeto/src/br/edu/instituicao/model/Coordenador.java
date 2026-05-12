5ee4c2741837b092c3a41292bc9421744332650e
Arthurz11 <arthurbpa29@gmail.com>
2026-05-12 18:09:04 -0300

    Update Coordenador.java

--- sistema_academico (1)/projeto/src/br/edu/instituicao/model/Coordenador.java
+++ sistema_academico (1)/projeto/src/br/edu/instituicao/model/Coordenador.java
@@ -1,25 +1,24 @@
 package br.edu.instituicao.model;
 
+// [AULA] Especialização: Coordenador é um tipo ainda mais específico.
+// Ele herda tudo do Professor (que por sua vez já tem tudo da Pessoa).
 public class Coordenador extends Professor {
 
-    private static final int BONUS_HORAS = 20;
-
     public Coordenador(String nome, String cpf, String email, String siape, String senha) {
+        // [AULA] super(): Repassa os dados para o construtor do Professor.
         super(nome, cpf, email, siape, senha);
     }
 
-    // Polimorfismo: sobrescreve o método do Professor com bônus (Desafio Bônus)
+    // [AULA] Sobrescrita / Polimorfismo Dinâmico: O coordenador reescreve a regra de horas do Professor.
     @Override
-    public int getHorasComBonus() {
-        return getHorasAtividade() + BONUS_HORAS;
+    public int calcularHorasAtividade(int horasBase) {
+        int bonusGestao = 20;
+        // [AULA] 'super.metodo()': Reutilizamos a lógica existente no Pai (Professor) e somamos nosso bônus!
+        return super.calcularHorasAtividade(horasBase) + bonusGestao;
     }
 
     @Override
-    public String toString() {
-        return "[COORDENADOR] " + getNome()
-                + " | CPF: " + getCpf()
-                + " | Email: " + getEmail()
-                + " | SIAPE: " + getSiape()
-                + " | Horas (com bonus): " + getHorasComBonus();
+    public String exibirDetalhes() {
+        return String.format("[COORDENADOR] %s | SIAPE: %s", getNome(), getSiape());
     }
 }
